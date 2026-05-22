91cfe6448c3d5c79efd36e2829635399d6cf940e
Anshuman Vishwakarma <123162801+anshu213@users.noreply.github.com>
2026-05-22 11:14:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,418 +1,270 @@
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Arial, Helvetica, sans-serif;
-}
-body{
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #e6f0ff;
+let activeQuestions = [];
+ let levelQuestions = [];
+ let score = 0;
+ let answered = false;
+ let currentSectionIndex = 0;
 
-}
-.home-screen{
-    width: 90%;
-    max-width: 500px;
-    padding: 40px 30px;
-    border-radius: 24px;
-    text-align: center;
-    background: linear-gradient(135deg, #0f172a, #1e293b);
-    color: white;
-    box-shadow: 0 20px 60px rgba(0,0,0,0.4);
-    animation: fadeIn 0.5s ease;
-}
-.home-screen h1{
-    font-size: 42px;
-    margin-bottom: 10px;
-    letter-spacing: 2px;
-    text-shadow: 0 0 15px rgba(255,255,255,0.2);
-}
-.home-screen p{
-    color: #cbd5e1;
-    margin-bottom: 25px;
-    font-size: 16px;
+ const question = document.getElementById("question");
+
+const buttons = document.querySelectorAll(".option-btn");
+
+const nextBtn = document.getElementById("next-btn");
+
+const skipBtn = document.getElementById("skip-btn");
+
+let currentQuestion = 0;
+
+
+
+function loadQuestion(){
+
+    answered = false;
+
+    question.innerText = activeQuestions[currentQuestion].question;
+
+    buttons.forEach((button, index) => {
+
+        button.innerText = activeQuestions[currentQuestion].options[index];
+
+         button.style.background = "linear-gradient(135deg, #575759, #9b9da1)";
+         button.style.color = "white";
+        button.disabled = false; 
+    });
 }
 
-.level-btn{
-    width: 140px;
-    height: 70px;
-    border-radius: 14px;
-    border: none;
-    background: linear-gradient(145deg, #334155, #1e293b);
-    color: white;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
-    transition: 0.3s;
-    box-shadow: 0 10px 25px rgba(0,0,0,0.3);
-}
-.level-btn:hover{
-    transform: translateY(-5px) scale(1.05);
-    background: linear-gradient(145deg, #dc2626, #7f1d1d);
-    box-shadow: 0 15px 35px rgba(220,38,38,0.4);
-}
+// loadQuestion();
 
-.level-selection{
-    margin-top: 30px;
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-}
+buttons.forEach(button => {
 
-.level-btn{
-    padding: 15px 25px;
-    border: none;
-    border-radius: 12px;
-    background: navy;
-    color: white;
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: bold;
-    transition: 0.3s;
-    width: 120px;
-    height: 60px;
-}
+    button.addEventListener("click", () => {
 
-.level-btn:hover{
-    background: darkred;
-    transform: scale(1.05);
+        if(answered){return;}
+
+        answered = true;
+
+        const correctAnswer = activeQuestions[currentQuestion].answer;
+         buttons.forEach(btn => {
+            btn.disabled = true;
+        });
+ 
+        if(button.innerText === correctAnswer){
+
+    score++;
+
+    button.style.background = "green";
 }
-.quiz-container {
-  width: 90%;
-  max-width: 420px;
-  padding: 30px;
-  border-radius: 18px;
-  box-shadow: 0 10px 30px rgba(0,0,0,0.15);
-  animation: fadeIn 0.5s ease-in-out;
-  border: 1px solid rgba(0,0,0,0.05);
-  display: none;
-  color: white;
- background: linear-gradient(135deg, #7f3a0a, #f87e32,#fdbe66);
-    box-shadow: 0 15px 40px rgba(0,0,0,0.3);
-}
-@keyframes fadeIn{
-    from{
-        opacity: 0;
-        transform: translateY(20px);
+     else{
+
+            button.style.background = "red";
+
+            buttons.forEach(btn => {
+
+                if(btn.innerText === correctAnswer){
+
+                    btn.style.background = "green";
+
+                }
+
+            });
+
+        }
+
+    });
+
+});
+
+nextBtn.addEventListener("click", () => {
+
+    if (!answered) {
+
+        showPopup(
+            "Flashquizz says",
+            "Please select an answer before moving next."
+        );
+
+        return;
     }
 
-    to{
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-.quiz-container h1{
-    text-align: center;
-    margin-bottom: 10px;
-    font-size: 28px;
-    font-weight: 800;
-    color: #1d4ed8; 
-   
-    letter-spacing: 1px;
-   
-}
-#back-btn{
-    margin-bottom: 20px;
-    padding: 10px 15px;
-    border:3px, solid, rgba(15, 5, 5, 0.6);
-    border-radius: 6px;
-    background: rgb(203, 2, 42);
-    color: white;
-    cursor: pointer;
-    font-weight: bold;
-}
+    currentQuestion++;
 
-#back-btn:hover{
-    background: rgb(149, 1, 1);
-}
-.tagline{
-    text-align: center;
-    color: gray;
-    margin-bottom: 25px;
-}
-#question{
-    margin-bottom: 20px;
-}
-.options{
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-}
-.option-btn{
-    width: 100%;
-    padding: 15px;
-    margin: 7px 0;
-    border: none;
-    color: white;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    font-size: 15px;
-    font-weight: 600;
-    background: rgba(255, 255, 255, 0.15);
-    backdrop-filter: blur(5px);
-}
-.option-btn:hover{
-    transform: translateY(-2px);
-    box-shadow: 0 8px 20px rgba(76, 77, 80, 0.4);
-}
-.button-group{
-    display: flex;
-    gap: 15px;
-    margin-top: 20px;
-}
-#skip-btn{
-    flex: 1;
-    padding: 12px;
-    border: none;
-    color: white;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: 0.3s;
-    background: linear-gradient(135deg, #64748b, #334155);
-}
+    if (currentQuestion < activeQuestions.length) {
 
-#next-btn{
-    flex: 1;
-    padding: 12px;
-    border: none;
-    color: white;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: 0.3s;
-    background: linear-gradient(135deg, #20e4fa, #5a7266);
-}
-#skip-btn:hover,#next-btn:hover {
-    transform: translateY(-2px);
-    box-shadow: 0 8px 20px rgba(100,116,139,0.4);
-}
-.popup {
-    display: none;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-     background: rgba(0,0,0,0.45);
+        loadQuestion();
 
-    backdrop-filter: blur(6px);
+    } else {
 
-    justify-content: center;
+        showPopup(
+            "Section Completed",
+            `Your Score: ${score}/${activeQuestions.length}`
+        );
+        sectionButtons[currentSectionIndex].classList.add("completed");
 
-    align-items: center;
+        document.querySelector(".quiz-container")
+        .style.display = "none";
 
-    z-index: 1000;
-    /* background: rgba(33, 32, 32, 0.6);
-    justify-content: center;
-    align-items: center; */
-}
-
-
-.popup.show {
-    display: flex;
-}
-.popup-box {
-    background: linear-gradient(145deg, #1e293b, #0f172a);
-    color: white;
-    padding: 25px 20px;
-    border-radius: 16px;
-    text-align: center;
-    width: 300px;
-    box-shadow: 0 15px 40px rgba(0,0,0,0.6);
-    border: 1px solid rgba(255,255,255,0.1);
-    animation: popIn 0.2s ease-in-out;
-
-}
-@keyframes popIn {
-    from {
-        transform: scale(0.8);
-        opacity: 0;
-    }
-    to {
-        transform: scale(1);
-        opacity: 1;
-    }
-}
-.okbutton{
-    margin-top: 15px;
-    padding: 10px 18px;
-    border: none;
-    border-radius: 8px;
-    background: crimson;
-    color: white;
-    cursor: pointer;
-    font-weight: bold;
-    transition: 0.3s;
-}
-.okbutton:hover{
-    background: darkred;
-}
-.section-selection{
-
-    display: none;
-
-    justify-content: center;
-
-    flex-wrap: wrap;
-
-    gap: 20px;
-}
-
- .section-btn{
-
-    width: 90px;
-
-    height: 90px;
-
-    border-radius: 50%;
-
-    border: 2px solid rgba(255,255,255,0.15);
-
-    background: linear-gradient(145deg, #334155, #1e293b);
-
-    color: white;
-
-    cursor: pointer;
-
-    font-weight: bold;
-
-    font-size: 14px;
-
-    transition: all 0.3s ease;
-
-    box-shadow: 0 10px 20px rgba(0,0,0,0.25);
-}
-
-.section-btn:hover{
-
-    transform: scale(1.05);
-
-    background: darkred;
-}
-.section-container{
-
-    display: none;
-
-    width: 90%;
-
-    max-width: 650px;
-
-    padding: 35px;
-
-    border-radius: 24px;
-
-    background: linear-gradient(135deg, #0f172a, #1e293b);
-
-    box-shadow: 0 20px 50px rgba(0,0,0,0.4);
-
-    /* text-align: center; */
-
-    color: white;
-
-    animation: fadeIn 0.4s ease;
-}
-.section-container h2{
-    text-align: center;
-
-    margin-bottom: 30px;
-
-    font-size: 32px;
-
-    /* letter-spacing: 1px; */
-}
-.section-btn{
-
-    width: 80px;
-
-    height: 80px;
-
-    border-radius: 50%;
-
-    border: none;
-
-    background: #334155;
-
-    color: white;
-
-    cursor: pointer;
-
-    font-weight: bold;
-
-    transition: 0.3s;
-}
-.section-btn:hover{
-
-    transform: translateY(-5px) scale(1.08);
-
-    background: linear-gradient(145deg, #dc2626, #7f1d1d);
-
-    box-shadow: 0 15px 30px rgba(220,38,38,0.4);
-}
-
-#section-back-btn{
-    display: block;
-
-    margin-bottom: 20px;
-
-    padding: 12px 20px;
-
-    border: none;
-
-    border-radius: 10px;
-
-    background: crimson;
-
-    color: white;
-
-    cursor: pointer;
-
-    font-weight: bold;
-
-    transition: 0.3s;
-}
-
-#section-back-btn:hover{
-
-    transform: scale(1.05);
-
-    background: darkred;
-}
-#section-back-btn{
- background: crimson;
-
-}
-#section-back-btn:hover{
-   background: brown;
-}
-.section-btn.completed{
-
-    background: linear-gradient(145deg, #16a34a, #166534);
-
-    border: 2px solid #4ade80;
-
-    color: white;
-}
-@media (max-width: 600px){
-
-    .home-screen{
-        width: 95%;
-        padding: 25px 15px;
+        document.querySelector(".section-container")
+        .style.display = "block";
     }
 
-    .level-btn{
-        width: 45%;
-        height: 60px;
-        font-size: 14px;
+});
+
+skipBtn.addEventListener("click", () => {
+
+    currentQuestion++;
+
+    if (currentQuestion < activeQuestions.length) {
+        loadQuestion();
+    } else {
+        
+        currentQuestion = 0;
+        
     }
 
-    .option-btn{
-        font-size: 14px;
-        padding: 12px;
-    }
+});
 
-    .quiz-container{
-        padding: 20px;
-    }
+
+function showPopup(title, message) {
+    document.getElementById("popup-title").innerText = title;
+    document.getElementById("popup-message").innerText = message;
+    document.getElementById("popup").style.display = "flex";
 }
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+const homeScreen =
+document.querySelector(".home-screen");
+
+const levelButtons =
+document.querySelectorAll(".level-btn");
+
+const sectionButtons =
+document.querySelectorAll(".section-btn");
+
+levelButtons.forEach((button, index) => {
+
+    button.addEventListener("click", () => {
+
+        const start = index * 100;
+
+        const end = start + 100;
+
+       levelQuestions =
+questions.slice(start, end);
+         if(levelQuestions.length === 0){
+            showPopup("No Questions", "This level is empty.");
+            return;
+        }
+
+        currentQuestion = 0;
+
+       homeScreen.style.display = "none";
+
+document.querySelector(".section-container")
+.style.display = "block";
+
+document.querySelector(".section-selection")
+.style.display = "flex";
+
+    });
+
+});
+sectionButtons.forEach((button, index) => {
+
+    button.addEventListener("click", () => {
+        currentSectionIndex = index;
+
+        const start = index * 10;
+
+        const end = start + 10;
+
+        activeQuestions =
+        levelQuestions.slice(start, end);
+
+        if(activeQuestions.length === 0){
+            showPopup("No Questions", "This section is empty.");
+            return;
+        }
+
+        currentQuestion = 0;
+        score = 0;
+
+        document.querySelector(".section-container")
+        .style.display = "none";
+
+        document.querySelector(".quiz-container")
+        .style.display = "block";
+
+        loadQuestion();
+
+    });
+
+});
+
+
+
+const backBtn =
+document.getElementById("back-btn");
+
+backBtn.addEventListener("click", () => {
+
+    document.querySelector(".quiz-container")
+    .style.display = "none";
+
+   document.querySelector(".section-container")
+.style.display = "block";
+
+document.querySelector(".section-selection")
+.style.display = "flex";
+
+    resetQuiz();
+
+});
+const sectionBackBtn =
+document.getElementById("section-back-btn");
+
+sectionBackBtn.addEventListener("click", () => {
+
+    document.querySelector(".section-container")
+    .style.display = "none";
+
+    homeScreen.style.display = "block";
+
+});
+
+function resetQuiz() {
+    currentQuestion = 0;
+    answered = false;
+
+    closePopup();
+
+    // reset buttons fully
+    buttons.forEach(btn => {
+        btn.disabled = false;
+        btn.style.background = "linear-gradient(135deg, #575759, #9b9da1)";
+        btn.style.color = "white";
+    });
+
+    // reload first question
+    loadQuestion();
+}
+
+
+
+
+
+  
+
+
+ 
+
+ 
+
+  
+  
+
+
+
+
+  
