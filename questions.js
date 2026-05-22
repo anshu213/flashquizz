40b2295d07b6e684e53c546813740e3f4d941032
Anshuman Vishwakarma <123162801+anshu213@users.noreply.github.com>
2026-05-22 11:15:55 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,354 @@
+const questions = [
+
+    {
+        question: "Q: Which country has the longest coastline in the world?",
+
+        options: [
+            "Russia",
+            "Canada",
+            "Australia",
+            "Indonesia"
+        ],
+
+        answer: "Canada"
+    },
+    
+    {
+        question: "Q: Who wrote the book '1984'?",
+
+        options: [
+            "George Orwell",
+            "William Shakespeare",
+            "J. K. Rowling",
+            "Ernest Hemingway"
+        ],
+
+        answer: "George Orwell"
+    },
+    {
+        question: "Q: Which Scientist proposed the Theory of Relativity?",
+
+        options: [
+            "Isaac Newton",
+            "Nikola Tesla",
+            "Albert Einstein",
+            "Galileo Galilei"
+        ],
+
+        answer: "Albert Einstein"
+    },
+    {
+        question: "Q: Which country is known as the Land of the Rising Sun? ",
+
+        options: [
+            "China",
+            "Japan",
+            "South Korea",
+            "Thailand"
+        ],
+
+        answer: "Japan"
+    },
+    {
+        question: "Q: Which is the smallest continent in the world? ",
+
+        options: [
+            "Europe", "Australia", "Antarctica", "South America"
+        ],
+
+        answer: "Australia"
+    },{
+        question: "Q: Who wrote the national anthem of India?",
+
+        options: [
+           "Bankim Chandra Chatterjee", "Rabindranath Tagore", "Mahatma Gandhi", "Subhash Chandra Bose"
+        ],
+
+        answer: "Rabindranath Tagore"
+    },{
+        question: "Q: Which is the hardest natural substance on Earth?",
+
+        options: [
+           "Gold", "Iron", "Diamond", "Platinum"],
+
+        answer: "Diamond"
+    },{
+        question: "Q: What is the study of the universe called? ",
+
+        options: [
+            "Astrophysics",
+            "Cosmology",
+            "Astronomy",
+            "Astrobiology"
+        ],
+
+        answer: "Cosmology"
+    },{
+        question: "Q: Who first proposed the idea of the Big Bang Theory",
+
+        options: [
+            "Edwin Hubble",
+            "Isaac Newton",
+            "Georges Lemaître",
+            "Albert Einstein"
+        ],
+
+        answer: "Georges Lemaître"
+    },{
+        question: "Q: What is the approximate age of the Earth",
+
+        options: [
+            "4.6 million years",
+            "4.6 billion years",
+            "460 million years",
+            "14 billion years"
+        ],
+
+        answer: "4.6 billion years"
+    },{
+        question: "Q: Which country adopted the first modern written constitution in 1787?",
+
+        options: [
+            "United Kingdom",
+            "United States of America",
+            "France",
+            "Germany"
+        ],
+
+        answer: "United States of America"
+    },{
+        question: "Q: Who is known as the father of Macroeconomics?",
+
+        options: [
+            "Adam Smith",
+            "John Maynard Keynes",
+            "Alfred Marshall",
+            "David Ricardo"
+        ],
+
+        answer: "John Maynard Keynes"
+    },{
+        question: "Q: When did the Delhi Sultanate rule in India?",
+
+        options: [
+            "750 AD – 1206 AD",
+            "1206 AD – 1526 AD",
+            "1526 AD – 1707 AD",
+            "1707 AD – 1947 AD"
+        ],
+
+        answer: "1206 AD – 1526 AD"
+    },{
+        question: "Q: Who came first to India among the Europeans? ",
+
+        options: [
+            "Portuguese",
+            "Dutch",
+            "German",
+            "French"
+        ],
+
+        answer: "Portuguese"
+    },{
+        question: "Q: What is motion?",
+
+        options: [
+            "Change in position of an object with respect to time and surrounding",
+            "Total path length covered by an object",
+            "Shortest distance between initial and final position",
+            "Rate of change of speed of an object"
+        ],
+
+        answer: "Change in position of an object with respect to time and surrounding"
+    },{
+        question: "Q: What is a substance?",
+
+        options: [
+            "A form of matter that has a uniform and definite composition",
+            "Anything that has mass and occupies space",
+            "A mixture of different materials",
+            "A state of matter like solid, liquid or gas"
+        ],
+
+        answer: "A form of matter that has a uniform and definite composition"
+    },{
+        question: "Q: What is the study of cells called?",
+
+        options: [
+            "Morphology",
+            "Histology",
+            "Physiology",
+            "Cytology"
+        ],
+
+        answer: "Cytology"
+    },{
+        question: "Q: Who is known as the Father of Indian Ecology?",
+
+        options: [
+            "Salim Ali",
+            "Ramdeo Misra",
+            "M. S. Swaminathan",
+            "Hargobind Khurana"
+        ],
+
+        answer: "Ramdeo Misra"
+    },{
+        question: "Q: Where is the headquarters of Sangeet Natak Akademi located?",
+
+        options: [
+            "Mumbai",
+            "Chennai",
+            "New Delhi",
+            "Kolkata"
+        ],
+
+        answer: "New Delhi"
+    },{
+        question: "Q: Which is the oldest classical dance form of India?",
+
+        options: [
+            "Kathak",
+            "Bharatanatyam",
+            "Kathakali",
+            "Odissi"
+        ],
+
+        answer: "Bharatanatyam"
+    },{
+        question:"Q: Which type of galaxy is the Milky Way?",
+
+        options: [
+          "Elliptical",
+          "Irregular",
+          "Spiral",
+          "Ring"
+          
+        ],
+
+        answer: "Spiral" 
+    },{
+        question:"Q: During which Stone Age period did humans begin agriculture and domestication of animals?",
+
+        options: [
+          "Paleolithic Age",
+          "Mesolithic Age",
+          "Neolithic Age",
+          "Neolithic Age"
+          
+        ],
+
+        answer: "Neolithic Age"
+    },{
+        question:"Q: Which ancient Indian university was one of the world’s first great centers of learning?",
+
+        options: [
+          "Takshashila",
+          "Nalanda",
+          "Vikramashila",
+          "Vallabhi"
+          
+        ],
+
+        answer: "Nalanda"
+    },{
+        question:"Q: What was Flake Technology?",
+
+        options: [
+          "Making tools by melting stones",
+          "Making tools by breaking small flakes from large stones",
+          "Making tools from wood",
+          "Heating iron to make weapons"
+          
+        ],
+
+        answer: "Making tools by breaking small flakes from large stones"
+    },{
+        question:"Q: Which of the following is an example of an amorphous (अक्रिस्टलीय) solid?",
+
+        options: [
+          "Diamond",
+          "Sodium chloride",
+          "Quartz",
+          "Glass"
+          
+        ],
+
+        answer: "Glass"
+    },{
+        question:"Q: Who is considered the father of Microeconomics?",
+
+        options: [
+          "Adam Smith",
+          "Alfred Marshall",
+          "Karl Marx",
+          "John Maynard Keynes"
+          
+        ],
+
+        answer: "Alfred Marshall"
+    },{
+        question:"Q: Which of the following is an example of a scalar (अदिश) quantity?",
+
+        options: [
+          "Speed",
+          "Force",
+          "Velocity",
+          "Acceleration"
+          
+        ],
+
+        answer: "Speed"
+    },{
+        question:"Q: Who first used the term “cell” after observing dead cells in a thin slice of cork?",
+
+        options: [
+          "Antonie van Leeuwenhoek",
+          "Louis Pasteur",
+          "Robert Hooke",
+          "Charles Darwin"
+          
+        ],
+
+        answer: "Robert Hooke"
+    },{
+        question:"Q: Who is known as the Father of Modern Ecology??",
+
+        options: [
+          "Charles Darwin",
+          "Ernst Haeckel",
+          "Gregor Mendel",
+          "Eugene P. Odum"
+          
+        ],
+
+        answer: "Eugene P. Odum"
+    },{
+        question:"Q: What was the earlier name of Bharatanatyam?",
+
+        options: [
+          "Dasi Attam",
+          "Sadir Attam",
+          "Natya Shastra Dance",
+          "Devadasi Nritya"
+          
+        ],
+
+        answer: "Dasi Attam"
+    },{
+        question:"Q: ?",
+
+        options: [
+          "",
+          "",
+          "",
+          ""
+          
+        ],
+
+        answer: ""
+        
+    }
+// ......................30 questions Done..................................
+
+];
